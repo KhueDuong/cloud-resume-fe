79c4b535408276e39ab8830f22b82a41cb86e40d
KhueDuong <duonghoangkhue.2004@gmail.com>
2024-12-29 16:49:54 +1100

    new S3 deploy work flow and disable ECS workflow

--- app/about_me/page.tsx
+++ app/about_me/page.tsx
@@ -16,13 +16,28 @@ const page = () => {
           Welcome to My Website! Check out the latest updates.
         </p>
       </div>
-      <div className="grid grid-rows-[60px_1fr_60px]  items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-cover bg-center bg-main-background">
-        <div className="bg-gray-200 p-10 rounded-lg shadow-lg mt-20">
-          <h1 className="text-7xl font-extrabold text-center text-indigo-600 drop-shadow-lg px-50 py-10">
+      <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-cover bg-center bg-main-background">
+        <div className="bg-gray-200 p-10 rounded-lg shadow-lg mt-10 max-w-3xl">
+          <h1 className="text-7xl font-extrabold text-center text-indigo-600 drop-shadow-lg px-50 py-3">
             About Me
           </h1>
           <div className="flex space-x-5 "></div>
           <br></br>
+          <div className="bg-gray-300 p-3 rounded-lg shadow-lg max-w-3xl">
+            <p className="text-lg text-gray-800 drop-shadow-lg">Hi!</p>
+            <br></br>
+            <p className="text-lg text-gray-800 drop-shadow-lg">
+              My real name is Duong Hoang Khue (Khue is my first name, Duong is
+              last name and Hoang is middle name). Because of the Vietnamese
+              naming system my name is flipped around like that, so you'll
+              usually see something like Hoang Khue Duong or even Hoang K. Duong
+              on my documents. So legally, my first name is Hoang Khue and
+              sometimes people call me Hoang even though technically thats my
+              middle name.
+            </p>
+            <div className="flex space-x-5 "></div>
+            <br></br>
+          </div>
         </div>
       </div>
     </>
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    distDir: "build",
-  };
+    //distDir: "build",
+    output: 'export',
+trailingSlash: true};
 
 export default nextConfig;
